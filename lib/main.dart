00be433755788a77ca53c93925b407c44ffb35b4
HMasataka <siruba.2050@gmail.com>
2024-04-14 18:00:32 +0900

    Feat add todo page

--- lib/main.dart
+++ lib/main.dart
@@ -23,15 +23,46 @@ class FTodo extends StatelessWidget {
   }
 }
 
-// リスト一覧画面用Widget
 class TodoListPage extends StatelessWidget {
   const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return const TodoAddPage();
+            }),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// リスト追加画面用Widget
+class TodoAddPage extends StatelessWidget {
+  const TodoAddPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: const Text('リスト追加画面（クリックで戻る）'),
+        ),
       ),
     );
   }
